8b8d39404a71ae36d14604f49ce5c0a1cd24b032
jisoonew <pjsu200@naver.com>
2024-04-02 05:33:46 +0900

    파일명 파스칼 표기법 적용 및 파일 그룹화

---
+++ 스쿼드 도서관/userbookmark.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace 스쿼드_도서관
+{
+    public partial class userbookmark : UserControl
+    {
+        public userbookmark()
+        {
+            InitializeComponent();
+        }
+
+        private void userbookmark_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=qkrwltn5130!");
+                MySqlDataAdapter adapter = new MySqlDataAdapter("select * from  squad_library.userbookmark", connection);
+
+                connection.Open();
+
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "alluser");
+                dataGridView1.DataSource = ds.Tables["alluser"];
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void LoadData()
+        {
+            string sql = "Server=localhost;Port=3306;username=root;password=cse1234";
+            MySqlConnection con = new MySqlConnection(sql);
+            MySqlCommand cmd_db = new MySqlCommand("SELECT  도서명,글쓴이,출판일,출판사 FROM squad_library.userbookmark;", con);
+
+            try
+            {
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmd_db;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rowIndext = dataGridView1.CurrentCell.RowIndex;
+            dataGridView1.Rows.RemoveAt(rowIndext);
+            string myconnect = "datasource = localhost; port = 3306; username=root; password=cse1234";
+            string query = "delete from squad_library.userbookmark 도서명";
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            MySqlConnection myconn = new MySqlConnection(myconnect);
+            MySqlCommand mycommand = new MySqlCommand(query, myconn);
+            MySqlDataReader myReader;
+
+            try
+            {
+                myconn.Open();
+                myReader = mycommand.ExecuteReader();
+                MessageBox.Show("삭제완료");
+                while (myReader.Read())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            LoadData();
+        }
+    }
+}
---
+++ 스쿼드 도서관/usersearch2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace 스쿼드_도서관
+{
+    public partial class usersearch2 : Form
+    {
+        public usersearch2()
+        {
+            InitializeComponent();
+        }
+
+        private void usersearch2_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlConnection conn = new MySqlConnection("datasource=localhost; port=3306; username=root; password=qkrwltn5130!");
+                MySqlDataAdapter adap = new MySqlDataAdapter("select * from squad_library.search1", conn);
+
+                conn.Open();
+
+                DataSet ds = new DataSet();
+                adap.Fill(ds, "alluser");
+                dataGridView1.DataSource = ds.Tables["alluser"];
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("회원정보 DB 연결에 실패하였습니다.");
+            }
+        }
+
+        private void 검색_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "도서명")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection("datasource=localhost; port=3306; username=root; password=qkrwltn5130!");  //DB 주소 가져오기
+                    MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT *  FROM squad_library.search1 where 도서명  LIKE  '%" + textBox1.Text + "%' ", connection);  // 콤보 박스 옆에 텍스트 박스 값 DB에 넣기
+
+                    connection.Open();  // DB 연결 시작
+
+                    DataSet ds = new DataSet();  //DataSet에 데이터 넣음
+                    adapter.Fill(ds, "alluser");  //search1 테이블 채우기
+                    dataGridView1.DataSource = ds.Tables["alluser"];  // 테이블 보이기
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("검색에 실패하였습니다.");
+                }
+            }
+            if (comboBox1.Text == "글쓴이")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection("datasource=localhost; port=3306; username=root; password=qkrwltn5130!");  //DB 주소 가져오기
+                    MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT *  FROM squad_library.search1 where 글쓴이 LIKE  '%" + textBox1.Text + "%' ", connection);  // 콤보 박스 옆에 텍스트 박스 값 DB에 넣기
+
+                    connection.Open();  // DB 연결 시작
+
+                    DataSet ds = new DataSet();  //DataSet에 데이터 넣음
+                    adapter.Fill(ds, "alluser");  //search1 테이블 채우기
+                    dataGridView1.DataSource = ds.Tables["alluser"];  // 테이블 보이기
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("검색에 실패하였습니다.");
+                }
+            }
+            if (comboBox1.Text == "출판사")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection("datasource=localhost; port=3306; username=root; password=qkrwltn5130!");  //DB 주소 가져오기
+                    MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT *  FROM squad_library.search1 where 출판사 LIKE  '%" + textBox1.Text + "%' ", connection);  // 콤보 박스 옆에 텍스트 박스 값 DB에 넣기
+
+                    connection.Open();  // DB 연결 시작
+
+                    DataSet ds = new DataSet();  //DataSet에 데이터 넣음
+                    adapter.Fill(ds, "alluser");  //search1 테이블 채우기
+                    dataGridView1.DataSource = ds.Tables["alluser"];  // 테이블 보이기
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("검색에 실패하였습니다.");
+                }
+            }
+        }
+    }
+}
